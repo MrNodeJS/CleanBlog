5a7859c37aec99d24c5faac2de4759d4b6e33175
MrNodeJS <nxtime.x@gmail.com>
2022-07-29 01:56:11 +0300

    Static Files Upload

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const express = require('express');
+const ejs     = require('ejs');
+const app     = express();
 
-const app = express()
+//Template Engine
+app.set("view engine", "ejs");
+
+
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-	const blog = {id: 1, title: "Blog title", description: "Blog description"}
+	res.render("index");
+});
+app.get('/about', (req, res) => {
+	res.render("about");
+});
+app.get('/add_post', (req, res) => {
+	res.render("add_post");
+});
+app.get('/post', (req, res) => {
+	res.render("post");
+});
 
-	res.send(blog)
-})
-
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-	console.log(`Sunucu ${port} portunda başladı...`)
-})
+	console.log(`Sunucu ${port} portunda başladı...`);
+});
